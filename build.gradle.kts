459327aed9f4db3fbd4f2c90b86e312cd878aeb0
Dmitriy Novozhilov <dmitriy.novozhilov@jetbrains.com>
2022-09-25 23:12:59 +0300

    Create project template

---
+++ build.gradle.kts
@@ -0,0 +1,45 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    java
+    kotlin("jvm") version "1.7.10"
+    application
+}
+
+group = "org.itmo.java"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.jetbrains:annotations:23.0.0")
+    testImplementation(platform("org.junit:junit-bom:5.8.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+sourceSets {
+    main {
+        java.setSrcDirs(listOf("src"))
+        resources.setSrcDirs(listOf("resources"))
+    }
+    test {
+        java.setSrcDirs(listOf("test"))
+        resources.setSrcDirs(listOf("testResources"))
+    }
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+java.toolchain {
+    languageVersion.set(JavaLanguageVersion.of(17))
+}
+
+val compileKotlin: KotlinCompile by tasks
+
+compileKotlin.kotlinOptions {
+    freeCompilerArgs += "-Xuse-k2"
+}
